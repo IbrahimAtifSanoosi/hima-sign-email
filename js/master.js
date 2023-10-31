aedb11afc90a11d18bd37878ef6e7d83e3d3d933
IbrahimAtifSanoosi <76850945+IbrahimAtifSanoosi@users.noreply.github.com>
2023-10-31 20:12:09 +0200

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -5,9 +5,13 @@ const errParagraph = document.querySelector(".erorr-msg");
 const template = document.querySelector(".template");
 const confirm = document.querySelector(".confirm");
 
-btn.addEventListener("click", (e) => {
-    if (valiaEmail(userInput)) {
+btn.addEventListener("click", checkEmail);
+
+function checkEmail() {
+    if (valiateEmail(userInput)) {
         console.log("Validate");
+        errParagraph.style.cssText = "display:none";
+        userInput.style = "border: 3px solid --dark-slate-grey ; background - color: --White";
         template.style.cssText = "display:none";
         confirm.style.cssText = "display:flex";
     }
@@ -16,14 +20,14 @@ btn.addEventListener("click", (e) => {
         errParagraph.style.cssText = "display:flex";
         userInput.style = "border: 3px solid rgb(245, 182, 182) ; background - color: rgb(245, 182, 182)";
     }
-});
+}
 
 dismissBtn.addEventListener("click", (e) => {
     template.style.cssText = "display:flex";
     confirm.style.cssText = "display:none";
 });
 
-function valiaEmail(userInput) {
+function valiateEmail(userInput) {
     var validReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if (userInput.value.match(validReg)) {
         return true;
