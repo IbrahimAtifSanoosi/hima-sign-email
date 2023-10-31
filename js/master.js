238c481f6d57d0864cd2ecd252e29100d783fe64
IbrahimAtifSanoosi <76850945+IbrahimAtifSanoosi@users.noreply.github.com>
2023-10-31 19:25:13 +0200

    Add files via upload

--- js/master.js
+++ js/master.js
@@ -1,13 +1,21 @@
-let userInput = document.querySelector(".input");
-let btn = document.querySelector(".btn");
-let dismissBtn = document.querySelector(".dismiss-btn");
-let errParagraph = document.querySelector(".erorr-msg");
-let errInput = document.querySelector(".erorr-input");
-let template = document.querySelector(".template");
-let confirm = document.querySelector(".confirm");
+const userInput = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const dismissBtn = document.querySelector(".dismiss-btn");
+const errParagraph = document.querySelector(".erorr-msg");
+const template = document.querySelector(".template");
+const confirm = document.querySelector(".confirm");
 
 btn.addEventListener("click", (e) => {
-    valiaEmail();
+    if (valiaEmail(userInput)) {
+        console.log("Validate");
+        template.style.cssText = "display:none";
+        confirm.style.cssText = "display:flex";
+    }
+    else {
+        console.log("Not Validate");
+        errParagraph.style.cssText = "display:flex";
+        userInput.style = "border: 3px solid rgb(245, 182, 182) ; background - color: rgb(245, 182, 182)";
+    }
 });
 
 dismissBtn.addEventListener("click", (e) => {
@@ -15,20 +23,13 @@ dismissBtn.addEventListener("click", (e) => {
     confirm.style.cssText = "display:none";
 });
 
-function valiaEmail() {
+function valiaEmail(userInput) {
     var validReg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    console.log(userInput.value.match(validReg));
-
-
-    if (userInput.value.match(validReg) || errInput.value.match(validReg)) {
-        template.style.cssText = "display:none";
-        confirm.style.cssText = "display:flex";
+    if (userInput.value.match(validReg)) {
+        return true;
     }
     else {
-        userInput.style.cssText = "display:none";
-        errParagraph.style.cssText = "display:flex";
-        // errInput.value = userInput.value;
-        errInput.style.cssText = "display:flex";
+        return false;
     }
 
 }
